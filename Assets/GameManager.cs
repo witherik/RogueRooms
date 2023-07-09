4426d6156c60e830f78004bc0ac729a8dd3bc16d
Dasyel <dasyelwillems@gmail.com>
2023-07-09 14:08:07 +0200

    Add working menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     [SerializeField] private Room[] rooms;
     [SerializeField] private EnemySpec[] enemySpecs;
     [SerializeField] private Hero hero;
     [SerializeField] private Camera cam;
+    [SerializeField] private RoomMenu roomMenu;
 
     private List<Enemy> enemies = new List<Enemy>();
     private List<Projectile> enemyProjectiles = new List<Projectile>();
 
-    private void Start()
-    {
-        InitRoom(2, 5);
+    private void Start() {
+        roomMenu.Show(4, 10);
     }
 
-    private void InitRoom(int numberOfEnemies, int powerLevel)
-    {
+    private void InitRoom(int numberOfEnemies, int powerLevel) {
         var room = rooms[Random.Range(0, rooms.Length)];
-        for (var i = 0; i < numberOfEnemies; i++)
-        {
+        for (var i = 0; i < numberOfEnemies; i++) {
             var enemySpec = enemySpecs[Random.Range(0, enemySpecs.Length)];
             var spawnPoint = room.enemySpawnPoints[i % room.enemySpawnPoints.Length];
             var enemy = Instantiate(enemySpec.prefab, spawnPoint.position, Quaternion.identity);
@@ -36,32 +33,31 @@ public class GameManager : MonoBehaviour
         cam.transform.position = new Vector3(newCamPos.x, newCamPos.y, cam.transform.position.z);
     }
 
-    public void OnProjectileSpawn(Projectile projectile)
-    {
+    public void StartRoom() {
+        InitRoom(2, 5);
+    }
+
+    public void OnProjectileSpawn(Projectile projectile) {
         enemyProjectiles.Add(projectile);
     }
-    public void OnProjectileDeath(Projectile projectile)
-    {
+
+    public void OnProjectileDeath(Projectile projectile) {
         enemyProjectiles.Remove(projectile);
     }
 
-    public void OnEnemySpawn(Enemy enemy)
-    {
+    public void OnEnemySpawn(Enemy enemy) {
         enemies.Add(enemy);
     }
 
-    public void OnEnemyDeath(Enemy enemy)
-    {
+    public void OnEnemyDeath(Enemy enemy) {
         enemies.Remove(enemy);
     }
 
-    public List<Enemy> GetEnemyList()
-    {
+    public List<Enemy> GetEnemyList() {
         return enemies;
     }
-    public List<Projectile> GetProjectileList()
-    {
+
+    public List<Projectile> GetProjectileList() {
         return enemyProjectiles;
     }
-
 }
---
+++ Assets/RoomMenu.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RoomMenu : MonoBehaviour {
+    [SerializeField] private Slider monsterLevelSlider;
+    [SerializeField] private RoomMenuItem itemOptionPrefab;
+    [SerializeField] private ItemOption[] itemOptions;
+    [SerializeField] private Transform itemOptionsParent;
+    [SerializeField] private GameManager gameManager;
+
+    private List<Tuple<int, ItemOption>> _weightedOptions;
+    private int _totalOptionWeight;
+    private List<RoomMenuItem> _itemOptionInstances = new();
+    private bool _isInitialized = false;
+    private List<Tuple<int, ItemOption>> _currentItemOptions = new();
+    private int _baseMonsterLevel = 0;
+    private int _currentMonsterLevel = 0;
+        
+    private void Init() {
+        _weightedOptions = new List<Tuple<int, ItemOption>>();
+        var runningWeightSum = 0;
+        foreach (var itemOption in itemOptions) {
+            _weightedOptions.Add(new Tuple<int, ItemOption>(runningWeightSum, itemOption));
+            runningWeightSum += itemOption.weight;
+        }
+
+        _totalOptionWeight = runningWeightSum;
+    }
+
+    public void Show(int numberOfItems, int baseMonsterLevel) {
+        if (!_isInitialized) {
+            Init();
+            _isInitialized = true;
+        }
+        foreach (var itemOptionInstance in _itemOptionInstances) {
+            Destroy(itemOptionInstance.gameObject);
+        }
+        _itemOptionInstances.Clear();
+
+        _baseMonsterLevel = baseMonsterLevel;
+        _currentMonsterLevel = baseMonsterLevel;
+        monsterLevelSlider.value = (float)baseMonsterLevel / 100;
+        
+        var pickedItems = new ItemOption[numberOfItems];
+        for (var i = 0; i < numberOfItems; i++) {
+            var randomWeightIndex = Random.Range(0, _totalOptionWeight);
+            var pickedItem = _weightedOptions.LastOrDefault(tuple => tuple.Item1 <= randomWeightIndex && !pickedItems.Contains(tuple.Item2));
+            while (pickedItem == null) { //ugly code to fix rare case where no items can be found
+                randomWeightIndex = Random.Range(0, _totalOptionWeight);
+                pickedItem = _weightedOptions.LastOrDefault(tuple => tuple.Item1 <= randomWeightIndex && !pickedItems.Contains(tuple.Item2));
+            }
+
+            pickedItems[i] = pickedItem.Item2;
+        }
+
+        _currentItemOptions.Clear();
+        for (var index = 0; index < pickedItems.Length; index++) {
+            var pickedItem = pickedItems[index];
+            var optionInstance = Instantiate(itemOptionPrefab, itemOptionsParent);
+            optionInstance.Init(pickedItem.icon, pickedItem.description, this, index);
+            _itemOptionInstances.Add(optionInstance);
+            _currentItemOptions.Add(new Tuple<int, ItemOption>(0, pickedItem));
+        }
+
+        gameObject.SetActive(true);
+    }
+
+    public void OnSubmit() {
+        Debug.Log("Submitted");
+        gameManager.StartRoom();
+        gameObject.SetActive(false);
+    }
+
+    public void OnPercentageChanged(int itemIndex, int percentage) {
+        Debug.Log($"Item {itemIndex} now has value {percentage}%");
+        var previousValue = _currentItemOptions[itemIndex];
+        _currentItemOptions[itemIndex] = new Tuple<int, ItemOption>(percentage, previousValue.Item2);
+        RecalculateMonsterLevel();
+    }
+
+    private void RecalculateMonsterLevel() {
+        var resultingLevel = (float)_baseMonsterLevel;
+        foreach (var currentItemOption in _currentItemOptions) {
+            var percentage = currentItemOption.Item1;
+            var multiplier = 0.25f;
+            while (percentage > 0) {
+                if (percentage >= 20) {
+                    resultingLevel += 20 * multiplier;
+                } else {
+                    resultingLevel += percentage * multiplier;
+                }
+                percentage -= 20;
+                multiplier += 0.25f;
+            }
+        }
+
+        _currentMonsterLevel = Mathf.Clamp((int)resultingLevel, 0, 100);
+        monsterLevelSlider.value = (float)_currentMonsterLevel / 100;
+        Debug.Log($"Total monster value {resultingLevel} -> {_currentMonsterLevel}");
+    }
+}
+
+[Serializable]
+public struct ItemOption {
+    public Sprite icon;
+    public string description;
+    public int weight;
+}
